7ca951a5182337481e7953241b501c951feba5fe
Robin Sola <rsolarms@gmail.com>
2017-12-05 16:56:42 -0800

    add a sumbit order button and create variables for all input to be placed into the object in javascript.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,11 @@
-function Pizza(size, crust, cheese, meat, topping) {
+// business logic
+
+function Pizza(size, crust, cheese, meats, toppings) {
   this.size = size;
   this.crust = crust;
   this.cheese = cheese;
-  this.meat = meat;
-  this.topping = topping;
+  this.meats = meats;
+  this.toppings = toppings;
   this.customer = [];
 }
 
@@ -16,3 +18,31 @@ function Customer(name, credit, address) {
 Pizza.prototype.cost = function() {
 
 }
+
+
+// user logic
+$(document).ready(function() {
+
+  $("form#order").submit(function(event) {
+    event.preventDefault();
+
+    debugger;
+    var inputSize = $("input:radio[name=size]:checked").val();
+    var inputCrust = $("input:radio[name=crust]:checked").val();
+    var inputCheese = $("input:radio[name=cheese]:checked").val();
+    var inputMeats = [];
+    var inputToppings = [];
+    $("input:checkbox[name=meat]:checked").each(function() {
+      var meatsChecked = $(this).val();
+      inputMeats.push(meatsChecked);
+    });
+    $("input:checkbox[name=top]:checked").each(function() {
+      var toppingsChecked = $(this).val();
+      inputToppings.push(toppingsChecked);
+    });
+
+    var newPizza = new Pizza(inputSize, inputCrust, inputCheese, inputMeats, inputToppings);
+
+    console.log(newPizza);
+  });
+});
