e8011d0ba65834823b17e660a1725d538f2690d9
Robin Sola <rsolarms@gmail.com>
2017-12-09 14:19:32 -0800

    hide form and fade in pizza ordered and add background images in css and organize form questions in div wells.

--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,10 @@ function Customer(name, credit, address) {
   this.pizzas = [];
 }
 
-function Pizza(size, crust, cheese, meats, toppings) {
+function Pizza(size, crust, cheez, meats, toppings) {
   this.size = size;
   this.crust = crust;
-  this.cheese = cheese;
+  this.cheez = cheez;
   this.meats = meats;
   this.toppings = toppings;
 }
@@ -19,13 +19,13 @@ Customer.prototype.info = function() {
 }
 
 Pizza.prototype.listOrder = function() {
-  return this.size + " " + this.crust + " crust " + this.cheese + " cheese " + this.meats.length + " meats and " + this.toppings.length + " toppings";
+  return this.size + " " + this.crust + " crust, " + this.cheez + " cheez, " + this.meats.length + " meats, and " + this.toppings.length + " toppings";
 }
 
 
 Pizza.prototype.cost = function() {
   var crustCost = 0;
-  var cheeseCost = 0;
+  var cheezCost = 0;
   var meatsCost = this.meats.length * 2;
   var toppingsCost = this.toppings.length * .50;
   if (this.size === "large") {
@@ -36,9 +36,9 @@ Pizza.prototype.cost = function() {
     crustCost += 6;
   }
   if (this.cheese === "extra") {
-    cheeseCost += 1;
+    cheezCost += 1;
   }
-  return crustCost + cheeseCost + meatsCost + toppingsCost;
+  return crustCost + cheezCost + meatsCost + toppingsCost;
 };
 
 
@@ -51,7 +51,7 @@ $(document).ready(function() {
     // user input variables for Pizza Object
     var inputSize = $("input:radio[name=size]:checked").val();
     var inputCrust = $("input:radio[name=crust]:checked").val();
-    var inputCheese = $("input:radio[name=cheese]:checked").val();
+    var inputCheez = $("input:radio[name=cheez]:checked").val();
     var inputMeats = [];
     var inputToppings = [];
     $("input:checkbox[name=meat]:checked").each(function() {
@@ -68,12 +68,14 @@ $(document).ready(function() {
     var inputCredit = $("#credit").val();
     var inputAddress = $("#address").val();
 
-    var newPizza = new Pizza(inputSize, inputCrust, inputCheese, inputMeats, inputToppings);
+    var newPizza = new Pizza(inputSize, inputCrust, inputCheez, inputMeats, inputToppings);
 
     var newCustomer = new Customer(inputName, inputCredit, inputAddress);
 
-    // $("#orders").append("<li><span class='pizza-ordered'>" + "Thank you for your order, " + newCustomer.info() + "</span></li>");
-    $("#orders").append("<li><span class='pizza-ordered'>" + newPizza.listOrder() + "</span></li>");
-    $("#receipt").text("Total: $" + newPizza.cost());
+    $("form#order").hide();
+    $(".receipt").fadeIn();
+    $("#receipt-name").text("Thank you for your order, " + newCustomer.info());
+    $("#receipt-orders").text(newPizza.listOrder());
+    $("#receipt-total").text("Total: $" + newPizza.cost());
   });
 });
